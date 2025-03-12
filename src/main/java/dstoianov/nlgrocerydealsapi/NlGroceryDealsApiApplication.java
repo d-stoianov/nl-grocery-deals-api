8536abbb4a1704e483792fc32126b96a70e0ebb8
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-03-12 23:35:05 +0100

    Set up the project
    Initialize the project with Spring Boot, Spring Web, PostgreSQL, Maven.

---
+++ src/main/java/dstoianov/nlgrocerydealsapi/NlGroceryDealsApiApplication.java
@@ -0,0 +1,13 @@
+package dstoianov.nlgrocerydealsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlGroceryDealsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlGroceryDealsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dstoianov/nlgrocerydealsapi/NlGroceryDealsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dstoianov.nlgrocerydealsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlGroceryDealsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
